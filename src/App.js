f073430f0fdba53dfb8473d613b509b55a7eff05
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-30 16:26:42 -0700

    Added a navbar component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react"
+import NavBar from "./components/layout/NavBar"
 
 import "./App.css"
 
@@ -6,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <h1>Hello</h1>
+        <NavBar />
       </div>
     )
   }
---
+++ src/components/layout/NavBar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className='navbar bg-primary'>
+        <h1>Navbar</h1>
+      </nav>
+    )
+  }
+}
+export default NavBar
