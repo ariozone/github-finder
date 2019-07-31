f6e78fa62d96308db4e96f3dd73693b6c46f701f
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-31 15:02:46 -0700

    Implementd loading spinner to render before user elements are mounted to the DOM.

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,20 +1,27 @@
-import React, { Component } from "react"
+import React from "react"
 import UserItem from "./UserItem"
+import Spinner from "../layout/spinner"
+import PropTypes from "prop-types"
 
-class User extends Component {
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.props.users.map(user => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    )
-  }
+const User = ({ users, loading }) => {
+  return loading ? (
+    <Spinner />
+  ) : (
+    <div style={userStyle}>
+      {users.map(user => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  )
 }
+
 const userStyle = {
   display: "grid",
   gridTemplateColumns: "repeat(3, 1fr)",
   gridGap: "1rem"
+}
+User.propTypes = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired
 }
 export default User
