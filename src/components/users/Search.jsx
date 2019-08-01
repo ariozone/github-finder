b46efe97b503ede24b311e167e2e5323fca1f785
Ario Tamaddoni <ariozone6@gmail.com>
2019-07-31 17:51:07 -0700

    Enhance the search feature to update the state.

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,14 +1,33 @@
 import React from "react"
 
 class Search extends React.Component {
+  state = {
+    searchQuery: ""
+  }
+  handleChange = e => {
+    const searchQuery = e.target.value
+    this.setState({ searchQuery })
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log(this.state.searchQuery)
+    this.setState({ searchQuery: "" })
+  }
   render() {
     return (
       <div>
-        <form className='form'>
-          <input type='text' name='search' placeholder='Search...' />
+        <form className='form' onSubmit={this.handleSubmit}>
           <input
+            type='text'
+            name='searchQuery'
+            placeholder='Search...'
+            value={this.state.searchQuery}
+            onChange={this.handleChange}
+          />
+          <input
+            id='search'
             type='submit'
-            value='search'
+            value='Search'
             className='btn btn-dark btn-block'
           />
         </form>
