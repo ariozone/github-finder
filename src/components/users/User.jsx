f0008ee4d0087c0a821cad6abe593224d6070bbc
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-02 16:56:13 -0700

    Enhanced the user page to show the user's main information.

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -22,6 +22,7 @@ class User extends Component {
       blog,
       login,
       html_url,
+      company,
       followers,
       following,
       public_repos,
@@ -35,6 +36,60 @@ class User extends Component {
         <Link to='/' className='btn btn-light'>
           Back to Search
         </Link>
+        Hireable:{" "}
+        {hireable ? (
+          <i className='fas fa-check text-success' />
+        ) : (
+          <i className='fas fa-times-circle text-danger' />
+        )}
+        <div className='card grid-2'>
+          <div className='all-center'>
+            <img
+              src={avatar_url}
+              alt='avatar'
+              className='round-img'
+              style={{ width: "150px" }}
+            />
+            <h1>{name}</h1>
+            <p>
+              Location: <strong>{location}</strong>
+            </p>
+          </div>
+          <div>
+            {bio && (
+              <Fragment>
+                <h3>Bio </h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a href={html_url} className='btn btn-dark my-1'>
+              Visit Github Profile
+            </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
       </Fragment>
     )
   }
