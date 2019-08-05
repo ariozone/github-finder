0936b6b097e0e7a6de2effe3797a001d23d5764a
Ario Tamaddoni <ariozone6@gmail.com>
2019-08-05 16:14:18 -0700

    Transform search class component to search functional component.

--- src/components/users/Search.jsx
+++ src/components/users/Search.jsx
@@ -1,63 +1,55 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-class Search extends React.Component {
-  state = {
-    searchQuery: ""
-  }
-
-  static propTypes = {
-    onSearch: PropTypes.func.isRequired,
-    onClear: PropTypes.func.isRequired,
-    showClear: PropTypes.bool.isRequired,
-    setAlert: PropTypes.func.isRequired
-  }
-
-  handleChange = e => {
+const Search = ({ onClear, setAlert, onSearch }) => {
+  const handleChange = e => {
     const searchQuery = e.target.value
     this.setState({ searchQuery })
   }
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault()
     if (this.state.searchQuery === "") {
-      this.props.setAlert("Please enter a search keyword!", "danger")
+      setAlert("Please enter a search keyword!", "danger")
     } else {
-      this.props.onSearch(this.state.searchQuery)
+      onSearch(this.state.searchQuery)
       this.setState({ searchQuery: "" })
     }
   }
 
-  handleClick = () => {
-    this.props.onClear()
+  const handleClick = () => {
+    onClear()
   }
-
-  render() {
-    return (
-      <div>
-        <form className='form' onSubmit={this.handleSubmit}>
-          <input
-            type='text'
-            name='searchQuery'
-            placeholder='Search...'
-            value={this.state.searchQuery}
-            onChange={this.handleChange}
-          />
-          <input
-            id='search'
-            type='submit'
-            value='Search'
-            className='btn btn-dark btn-block'
-          />
-        </form>
-        {this.props.showClear && (
-          <button className='btn light btn-block' onClick={this.handleClick}>
-            Clear
-          </button>
-        )}
-      </div>
-    )
-  }
+  return (
+    <div>
+      <form className='form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='searchQuery'
+          placeholder='Search...'
+          value={this.state.searchQuery}
+          onChange={handleChange}
+        />
+        <input
+          id='search'
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+      </form>
+      {this.props.showClear && (
+        <button className='btn light btn-block' onClick={handleClick}>
+          Clear
+        </button>
+      )}
+    </div>
+  )
 }
 
+Search.propTypes = {
+  onSearch: PropTypes.func.isRequired,
+  onClear: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  setAlert: PropTypes.func.isRequired
+}
 export default Search
