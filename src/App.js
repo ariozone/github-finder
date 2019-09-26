dcac27804862871915a0c8b303f406a2c45194c1
Ario Tamaddoni <ariozone6@gmail.com>
2019-09-25 19:35:14 -0700

    Added Home and Not Found pages to the app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React, { Fragment } from "react"
+import React from "react"
 import NavBar from "./components/layout/NavBar"
-import Users from "./components/users/Users"
-import Search from "./components/users/Search"
 import User from "./components/users/User"
 import Alert from "./components/layout/Alert"
 import About from "./components/pages/About"
+import Home from "./components/pages/Home"
+import NotFound from "./components/pages/NotFound"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import GithubState from "./contexts/github/GithubState"
 import AlertState from "./contexts/alert/AlertState"
@@ -20,18 +20,10 @@ const App = () => {
             <div className='container'>
               <Alert />
               <Switch>
-                <Route
-                  exact
-                  path='/'
-                  render={props => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound}></Route>
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Users from "../users/Users"
+import Search from "../users/Search"
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <Search></Search>
+      <Users></Users>
+    </React.Fragment>
+  )
+}
+export default Home
